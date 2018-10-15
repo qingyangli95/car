db1acf74dd4169f7e3d465d48bbeadad898eaaa0
VincentLiu92168 <44157437+VincentLiu92168@users.noreply.github.com>
2018-10-15 21:35:42 +1100

    Add files via upload
    Added attribute for visitedTiles and updatedMap

--- SWEN30006_2018S2_PartC/src/mycontroller/MyAIController.java
+++ SWEN30006_2018S2_PartC/src/mycontroller/MyAIController.java
@@ -1,12 +1,22 @@
 package mycontroller;
 
+import java.util.HashMap;
+
+import tiles.MapTile;
+import utilities.Coordinate;
 import controller.CarController;
 import world.Car;
 
 public class MyAIController extends CarController{
-
+	private HashMap<Coordinate, MapTile> visitedTiles; //tiles that have come into our view i.e updated
+	private HashMap<Coordinate, MapTile> updatedMap; //most up to date view of the entire map
+	
 	public MyAIController(Car car) {
 		super(car);
+		updatedMap = new HashMap<Coordinate, MapTile>();
+		updatedMap.putAll(getMap()); //copy in map
+		visitedTiles = new HashMap<Coordinate, MapTile>();
+		visitedTiles.putAll(getView()); //copy current starting view
 	}
 
 	@Override
