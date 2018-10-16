2fed658339e3c27c6e9073d92ac556e78f49dc10
VincentLiu92168 <44157437+VincentLiu92168@users.noreply.github.com>
2018-10-16 19:16:36 +1100

    Update MyAIController.java
    updated getCoord to have strategy and factory pattern

--- SWEN30006_2018S2_PartC/src/mycontroller/MyAIController.java
+++ SWEN30006_2018S2_PartC/src/mycontroller/MyAIController.java
@@ -19,8 +19,6 @@ public class MyAIController extends CarController{
 	
 	private double EPS = 1e-7;
 	
-	private enum State {FINDING_KEYS, FOUND_KEYS};
-	private State currentState;
 	private float Max_Speed=1;
 	private float Min_Speed=-1;
 	
@@ -30,17 +28,10 @@ public class MyAIController extends CarController{
 		updatedMap.putAll(getMap()); //copy in map
 		visitedTiles = new HashMap<Coordinate, MapTile>();
 		visitedTiles.putAll(getView()); //copy current starting view
-		currentState = State.FINDING_KEYS;
 	}
 
 	@Override
 	public void update() {
-		//update our state first
-		if (getKeys().size() == numKeys()) {
-			currentState = State.FOUND_KEYS;
-		} else {
-			currentState = State.FINDING_KEYS;
-		}
 		Coordinate destination = getCoordinate(visitedTiles, updatedMap);
 		
 	}
@@ -48,20 +39,19 @@ public class MyAIController extends CarController{
 	
 	private Coordinate getCoordinate(HashMap<Coordinate, MapTile> visitedTiles,
 			HashMap<Coordinate, MapTile> updatedMap) {
-		LinkedList<Coordinate> listOfTiles = new LinkedList<Coordinate>();
+		LinkedList<Coordinate> tiles = new LinkedList<Coordinate>();
 		
 		//insert coordinates
-		listOfTiles.addAll(updatedMap.keySet());
+		tiles.addAll(updatedMap.keySet());
 		
 		//prioritise unvisited tiles first, then use distance
-		listOfTiles.sort(new Comparator<Coordinate>() {
+		tiles.sort(new Comparator<Coordinate>() {
 
 			@Override
 			public int compare(Coordinate point1, Coordinate point2) {
-				Coordinate currentCoord = new Coordinate(getPosition());
 				
-				double distanceToPoint1 = distance(currentCoord, point1);
-				double distanceToPoint2 = distance(currentCoord, point2);
+				double distanceToPoint1 = distanceFrom(point1);
+				double distanceToPoint2 = distanceFrom(point2);
 				
 				boolean visitedPoint1 = visitedTiles.containsKey(point1);
 				boolean visitedPoint2 = visitedTiles.containsKey(point1);
@@ -74,52 +64,45 @@ public class MyAIController extends CarController{
 						return distanceToPoint1 < distanceToPoint2? 1:-1; //better to go to tile 1
 					}
 				} else {
-					return visitedPoint2?1:-1; //better to go to tile1 is already visited tile2
+					return visitedPoint2?1:-1; //better to go to tile1 since we already visited tile2
 				}
 				
 			}
 			
 		});
 		
-		//select tile coordinate based on our state
-		if (currentState.equals(State.FINDING_KEYS)) {
-			for (Coordinate coord: listOfTiles) {
-				MapTile currentTile = updatedMap.get(coord);
-				Type currentType = currentTile.getType();
-				//go to lava if it contains a key we need
-				if (currentType.equals(Type.TRAP) && ((TrapTile)currentTile).getTrap().equals("lava")) {
-					int lavaTileKey = ((LavaTrap) updatedMap.get(coord)).getKey();
-					if (lavaTileKey!=0 && !(getKeys().contains(lavaTileKey))) {
-						return coord;
-					}
-				}
-			}
-			//no lava to our knowledge.. just pick the closest road that isn't the same spot as
-			//the car.
-			for (Coordinate coord: listOfTiles) {
-				Type currentType = updatedMap.get(coord).getType();
-				Coordinate currentCoord = new Coordinate(getPosition());
-				if (currentType.equals(Type.ROAD) && (distance(currentCoord, coord)>EPS)) {
-					return coord;
-				}
-			}
-			//there should always be a road to go to
+		//use strategy pattern and concrete factory to select our tile
+		ITileSelector tileSelector = TileSelectorFactory.getInstance().getTileSelector(this);
+		Coordinate bestCoord = tileSelector.selectTile(tiles, this);
 		
-		} else if (currentState.equals(State.FOUND_KEYS)) {
-			//head to nearest exit
-			for (Coordinate coord: listOfTiles) {
-				if (updatedMap.get(coord).isType(Type.FINISH)) {
-					return coord;
-				}
-			}
+		//failed to select the most desirable tile, just go to default behaviour
+		if (bestCoord == null) {
+			tileSelector = TileSelectorFactory.getInstance().getDefaultTileSelector();
+			bestCoord = tileSelector.selectTile(tiles, this);
 		}
 		
-		return null;
+		return bestCoord;
 	}
 	
 	/** helper function to calculate simple Euclidean distance between coordinates */
 	private double distance(Coordinate point1, Coordinate point2) {
 		return Math.sqrt(Math.pow(point1.x-point2.x, 2) + Math.pow(point1.y-point2.y, 2));
+	}
+	
+	/** helper function to calculate distance from car to a point */
+	public double distanceFrom(Coordinate point) {
+		Coordinate currentCoord = new Coordinate(getPosition());
+		return distance(currentCoord, point);
+	}
+	
+	/** helper function to check whether we are on the same spot as a coordinate */
+	public boolean isLocatedAt(Coordinate coord) {
+		Coordinate ourCoord = new Coordinate(getPosition());
+		return ourCoord.equals(coord);
+	}
+
+	public HashMap<Coordinate, MapTile> getUpdatedMap() {
+		return updatedMap;
 	}
 
     /**
