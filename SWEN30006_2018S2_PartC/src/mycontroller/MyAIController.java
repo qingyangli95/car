29bd0f9f7edf31a5958deb460dd3760e070452fa
VincentLiu92168 <44157437+VincentLiu92168@users.noreply.github.com>
2018-10-17 19:06:35 +1100

    Add files via upload
    sketch of how to fix updateMap

--- SWEN30006_2018S2_PartC/src/mycontroller/MyAIController.java
+++ SWEN30006_2018S2_PartC/src/mycontroller/MyAIController.java
@@ -153,6 +153,74 @@ public class MyAIController extends CarController{
 		applyBrake(); //brake when arrived destination
 	}
 
+	ialises the map by putting all of the tiles into AugmentedMapTiles which
+	 * just have a boolean value attached to each tile to make path finding easier */
+	private void initMap() {
+		HashMap<Coordinate, MapTile> tempMap = getMap();
+		map = new HashMap<Coordinate, AugmentedMapTile>();
+		
+		for (Coordinate coord: tempMap.keySet()) {
+			MapTile = tempMap.get(coord);
+			
+			AugmentedMapTile tempMapTile = new AugmentedMapTile(MapTile, false);
+			map.put(coord, AugmentedTile)
+		}
+		 updatedMap();
+		
+		/** Get map dimensions */
+		int i, j;
+		for (i = 0; tempMap.containsKey(new Coordinate(i,0)); i++);
+		int mapWidth = i;
+		int mapHeight = tempMap.size()/i;
+		Coordinate n;
+		
+		/** copy contents of tempMap into map with the AugmentedMapTile */
+		for (i = 0; i <= mapWidth; i++) {
+			for (j = 0; j <= mapHeight; j++) {
+				n = new Coordinate(i, j);
+				map.put(n, new AugmentedMapTile(tempMap.get(n)));
+				
+			}
+		}
+	}
+	/** Updates the map with the current view from the car using getView() */
+	private void updateMap() {
+		HashMap<Coordinate, MapTile> view = getView();
+		//
+		//
+		//
+		/** Get the cars position and view square */
+		String[] posXY=getPosition().split(",");
+		int posX = Integer.parseInt(posXY[0]);
+		int posY = Integer.parseInt(posXY[1]);
+		int viewSquare = getViewSquare();
+       System.out.println("viewSquare:"+viewSquare);
+		Coordinate n;	// Helper coordinate to simplify code and aid readability
+
+       /**Update the current position is visited*/
+       n=new Coordinate(posX, posY);
+       if (map.containsKey(n)) {
+           map.get(n).setVisited(true);}
+
+		/** Update all tiles in map visible in view */
+		int i, j;
+		for (Coordinate coord: view.keySet()) {
+			map.get(coord).setVisited(true);
+		}
+		for (i = posX - viewSquare; i <= posX + viewSquare; i ++) {
+			for (j = posY - viewSquare; j <= posY + viewSquare; j++) {
+				n = new Coordinate(i, j);
+				if (map.containsKey(n)) {
+					map.get(n).setTile(view.get(n));
+					//System.out.println(n.toString() + map.get(n).getTile().getType().toString());
+				}
+			}
+		}
+	}
+	
+
+	
+}
 
 
 
